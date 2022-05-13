993c7f494097f989fdb870d6dd763acc8c70b9bd
liyaodev <liyaodev@qq.com>
2022-05-13 14:36:33 +0800

    add: discovery server idea

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 
 ## Python 编程实践
 
-* [插件化系统开发](./bootcamp/plugin_system/README.md)
-* [requests的使用](./bootcamp/requests/README.md)
-
+* [插件化系统开发](./bootcamp/plugin_system)
+* [requests的使用](./bootcamp/requests)
+* [分布式服务发现](./bootcamp/discovery_server/README.md)
---
+++ bootcamp/discovery_server/server.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+"""
+@created on 2022/03/26
+@author Yao Li
+"""
+
+import os
+import sys
+from os.path import dirname, abspath
+
+BASE_DIR = dirname(abspath(__file__))
+sys.path.insert(0, BASE_DIR)
+
+
+if __name__ == '__main__':
+
+    pass
